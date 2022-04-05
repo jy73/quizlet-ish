c071ce4a04f93c61c6b3b2b223bfaeb9345ce722
Jordan Yee <jordantyee@berkeley.edu>
2022-04-04 21:38:15 -0700

    Initial Commit

---
+++ quizlet-ish/quizlet_ishApp.swift
@@ -0,0 +1,17 @@
+//
+//  quizlet_ishApp.swift
+//  quizlet-ish
+//
+//  Created by Jordan Yee on 4/4/22.
+//
+
+import SwiftUI
+
+@main
+struct quizlet_ishApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
